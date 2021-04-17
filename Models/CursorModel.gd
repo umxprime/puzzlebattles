97f4bdc43776324ecc7b401545d09140b50b899d
Maxime CHAPELET <umxprime@gmail.com>
2021-04-17 23:15:00 +0200

    Evaluate diamonds after rotation and break if diamond contains single color

--- Models/CursorModel.gd
+++ Models/CursorModel.gd
@@ -21,17 +21,21 @@ func moveTo(direction:int) -> bool:
 		Enums.Direction.Right:
 			newPos.column += 1
 	return setPosition(newPos)
+func _columnsRange() -> PoolIntArray :
+	return PoolIntArray([1, _grid.columns() - 2])
+func _rowsRange() -> PoolIntArray :
+	return PoolIntArray([0, _grid.visibleRows() - 2])
 func setPosition(pos:GridCoordinate) -> bool:
-	if (pos.column < 1):
-		pos.column = 1
-	if (pos.column >= _grid.columns() - 1):
+	if (pos.column < _columnsRange()[0]):
+		pos.column = _columnsRange()[0]
+	if (pos.column > _columnsRange()[1]):
 		if pos.column%2==1 && pos.row > 0:
 			pos.row -=1
-		pos.column = _grid.columns() - 2
-	if (pos.row < 0) :
-		pos.row = 0
-	if (pos.row >= _grid.visibleRows() - 1):
-		pos.row = _grid.visibleRows() - 2
+		pos.column = _columnsRange()[1]
+	if (pos.row < _rowsRange()[0]) :
+		pos.row = _rowsRange()[0]
+	if (pos.row > _rowsRange()[1]):
+		pos.row = _rowsRange()[1]
 	if _pos.column == pos.column && _pos.row == pos.row:
 		return false
 	_pos.column = pos.column
--- Models/GridModel.gd
+++ Models/GridModel.gd
@@ -1,5 +1,20 @@
 class_name GridModel
 
+class Diamond:
+	var _down:BlockModel
+	var _left:BlockModel
+	var _top:BlockModel
+	var _right:BlockModel
+	func _init(down:BlockModel, left:BlockModel, top:BlockModel, right:BlockModel):
+		_down = down
+		_left = left
+		_top = top
+		_right = right
+	func blocks() -> Array:
+		return [_down, _left, _top, _right]
+	func haveSameType() -> bool:
+		return _down.type() == _left.type() && _left.type() == _top.type() && _top.type() == _right.type()
+
 var _blocks=[]
 var _columns:int
 var _rows:int
@@ -19,15 +34,39 @@ func visibleRows() -> int :
 	return _visibleRows
 func rows() -> int :
 	return _rows
-func blockAt(pos:GridCoordinate) -> BlockModel :
-	if (pos.row < 0 || pos.row + 1 > _rows || pos.column < 0 || pos.column + 1 > _columns) :
+func _blockAt(row, column) -> BlockModel :
+	if (row < 0 || row + 1 > _rows || column < 0 || column + 1 > _columns) :
 		return null
-	return _blocks[blockIndex(pos)]
+	return _blocks[_blockIndex(row, column)]
+func blockAt(pos:GridCoordinate) -> BlockModel :
+	return _blockAt(pos.row, pos.column)
+func _blockIndex(row, column) -> int:
+	return column + row * _columns
 func blockIndex(pos:GridCoordinate) -> int :
-	return pos.column + pos.row * _columns
+	return _blockIndex(pos.row, pos.column)
 func move(block:BlockModel, pos:GridCoordinate):
 	block.setPosition(pos)
 	_blocks[blockIndex(pos)] = block
+func _evaluate():
+	for row in range(0, visibleRows() - 2):
+		for column in range(1, columns() - 2):
+			var diamond: Diamond = _diamondAt(row, column)
+			if diamond == null : continue
+			if !diamond.haveSameType():
+				continue
+			for block in diamond.blocks():
+				breakSingle(block)
+			pass
+func _diamondAt(row, column) -> Diamond:
+	var down = _blockAt(row, column)
+	if down == null: return null
+	var top = down.blockAt(Enums.Location.Over)
+	if top == null: return null
+	var left = down.blockAt(Enums.Location.LeftSide)
+	if left == null: return null
+	var right = down.blockAt(Enums.Location.RightSide)
+	if right == null: return null
+	return Diamond.new(down, left, top, right)
 func cursor()->CursorModel:
 	return _cursor
 func swap(from:BlockModel, to:BlockModel):
@@ -72,6 +111,7 @@ func rotate(rotation:int):
 				swap(block,right)
 			_:
 				pass
+		_evaluate()
 func _fill():
 	var block:BlockModel
 	var pos:GridCoordinate
