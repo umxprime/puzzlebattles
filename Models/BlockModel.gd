e56b81d836e640fd8be3a0e4cef15e6cfb04c5ca
Maxime CHAPELET <umxprime@gmail.com>
2021-04-22 00:35:59 +0200

    Add boilerplate code for future FSM in block model

--- Models/BlockModel.gd
+++ Models/BlockModel.gd
@@ -1,6 +1,46 @@
 class_name BlockModel
 
+class BlockState:
+	var _model: BlockModel
+	func _init(model:BlockModel):
+		_model = model
+	func enter():pass
+	func handle(state:BlockState) -> BlockState:
+		return self
+	func exit():pass
+
+class BlockWaitState:
+	extends BlockState
+	func handle(state:BlockState) -> BlockState:
+		return state
+
+class BlockRotateState:
+	extends BlockState
+	var _rotated: bool = false
+	func enter():
+		pass
+	func handle(state:BlockState) -> BlockState:
+		if !_rotated : return self
+		return state
+
+class BlockBreakState:
+	extends BlockState
+	var _broken: bool = false
+	func enter():
+		pass
+	func handle(state:BlockState) -> BlockState:
+		if !_broken : return self
+		return state
+	func _finished():
+		_broken = true
+		_model.setState(BlockWaitState.new(_model))
+
+class BlockFallState:
+	extends BlockState
+
 var _state
+
+signal stateChanged(from, to)
 
 static func shuffleType()->int:
 	return randi() % (Enums.BlockType.U-Enums.BlockType.A+1) + Enums.BlockType.A
@@ -20,6 +60,16 @@ func _onViewReady():
 func node():
 	return _state.node
 	
+#func setState(state:BlockState) -> bool:
+#	var newState: BlockState = _state.handle(state)
+#	var stateChanged: bool = _state != newState
+#	if stateChanged:
+#		_state.exit()
+#		var previousState = _state
+#		_state = newState
+#		_state.enter()
+#		emit_signal("stateChanged", previousState, newState)
+#	return stateChanged
 func type()->int:
 	return _state.type
 func setType(type:int):
